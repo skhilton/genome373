c6f30fbd6652e1539c85d2b4e5e938e2595c4f87
Sarah Hilton <skhilton@uw.edu>
2019-03-23 13:04:38 -0700

    new pdf strategy

--- slides/process.py
+++ slides/process.py
@@ -11,7 +11,7 @@ import glob
 
 def make_md(title, nb, html, pdf, fname):
     header = f"---\nlayout: page\ntitle: {title}\n---\n"
-    body = f"* [`jupyter notebook`]({nb})\n* [`html`]({html})\n* [`pdf`]({pdf})\n"
+    body = f"* [`jupyter notebook`]({os.path.basename(nb)})\n* [`html`]({os.path.basename(html)})\n* [`pdf`]({os.path.basename(pdf)})\n"
     with open(fname, "w") as f:
         f.write(header)
         f.write(body)
@@ -30,25 +30,30 @@ def main():
             html_fname = f"{week_dir}{prefix}.slides.html"
             pdf_fname = f"{week_dir}{prefix}.pdf"
             md_fname = f"{week_dir}{prefix}.md"
+            toc_fname = f"{week_dir}{prefix}_toc.md"
             title = f"week {week} ({date})"
             # define commands
             html_CMD = ["jupyter", "nbconvert", nb, "--to", "slides"]
-            pdf_CMD = ["jupyter", "nbconvert", nb, "--to", "pdf"]
+            md_CMD = ["jupyter", "nbconvert", nb, "--to", "markdown"]
+            pdf_CMD = ["pandoc", md_fname, "--latex-engine=xelatex", "-o",
+                       pdf_fname, "-V", "geometry:margin=1in"]
             if clean:
                 subprocess.check_call(html_CMD)
+                subprocess.check_call(md_CMD)
                 subprocess.check_call(pdf_CMD)
-                make_md(title, nb, html_fname, pdf_fname, md_fname)
+                make_md(title, nb, html_fname, pdf_fname, toc_fname)
             else:
                 if not os.path.isfile(html_fname):
                     subprocess.check_call(html_CMD)
                 else:
                     print(f"Already found file {html_fname}")
                 if not os.path.isfile(pdf_fname):
+                    subprocess.check_call(md_CMD)
                     subprocess.check_call(pdf_CMD)
                 else:
                     print(f"Already found file {pdf_fname}")
                 if not os.path.isfile(md_fname):
-                    make_md(title, nb, html_fname, pdf_fname, md_fname)
+                    make_md(title, nb, html_fname, pdf_fname, toc_fname)
                 else:
                     print(f"Already found file {md_fname}")
 
---
+++ slides/week_1/week_1_4-4-19_toc.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: week 1 (4-4-19)
+---
+* [`jupyter notebook`](week_1_4-4-19.ipynb)
+* [`html`](week_1_4-4-19.slides.html)
+* [`pdf`](week_1_4-4-19.pdf)
