6e1fa59b554916e3d4e5f09935b575b76c4f74d3
Sarah Hilton <skhilton@uw.edu>
2019-03-23 11:47:43 -0700

    changed name formatting

--- slides/process.py
+++ slides/process.py
@@ -8,9 +8,9 @@ import os
 import subprocess
 import glob
 
-def make_md(title, nb, html, pdf, fname):
-    # header = f"---\nlayout: page\ntitle:{title}\npermalink: /slides/week\n---\n"
-    header = f"{title}\n"
+
+def make_md(nb, html, pdf, fname):
+    header = f"---\nlayout: post\n---\n"
     body = f"* [`jupyter notebook`]({nb})\n* [`html`]({html})\n* [`pdf`]({pdf})\n"
     with open(fname, "w") as f:
         f.write(header)
@@ -21,11 +21,9 @@ def make_md(title, nb, html, pdf, fname):
 def main():
     clean = False
     for week_dir in glob.glob("week*/"):
-        for nb in glob.glob(f"{week_dir}week*.ipynb"):
+        for nb in glob.glob(f"{week_dir}*.ipynb"):
             # extract info from name
             prefix = os.path.splitext(os.path.basename(nb))[0]
-            week = prefix.split("_")[1]
-            date = prefix.split("_")[2]
             # define output names
             html_fname = f"{week_dir}{prefix}.slides.html"
             pdf_fname = f"{week_dir}{prefix}.pdf"
@@ -36,7 +34,7 @@ def main():
             if clean:
                 subprocess.check_call(html_CMD)
                 subprocess.check_call(pdf_CMD)
-                make_md(f"week {week} ({date})", nb, html_fname, pdf_fname, md_fname)
+                make_md(nb, html_fname, pdf_fname, md_fname)
             else:
                 if not os.path.isfile(html_fname):
                     subprocess.check_call(html_CMD)
@@ -47,7 +45,7 @@ def main():
                 else:
                     print(f"Already found file {pdf_fname}")
                 if not os.path.isfile(md_fname):
-                    make_md(f"week {week} ({date})", nb, html_fname, pdf_fname, md_fname)
+                    make_md(nb, html_fname, pdf_fname, md_fname)
                 else:
                     print(f"Already found file {md_fname}")
 
