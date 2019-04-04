119edfd2b6a2b8a720b2d0203862e0aa57ffdbbc
Sarah Hilton <skhilton@uw.edu>
2019-04-03 21:46:21 -0700

    updated process script

--- slides/process.py
+++ slides/process.py
@@ -9,9 +9,9 @@ import subprocess
 import glob
 
 
-def make_md(title, nb, html, pdf, md, fname):
+def make_md(title, html, pdf, fname):
     header = f"---\nlayout: page\ntitle: {title}\n---\n"
-    body = f"* [`markdown`]({os.path.basename(md)})\n* [`html`]({os.path.basename(html)})\n* [`pdf`]({os.path.basename(pdf)})\n* [`jupyter notebook`]({os.path.basename(nb)})\n"
+    body = f"* [`html`]({os.path.basename(html)})\n* [`pdf`]({os.path.basename(pdf)})\n"
     with open(fname, "w") as f:
         f.write(header)
         f.write(body)
@@ -21,43 +21,42 @@ def make_md(title, nb, html, pdf, md, fname):
 def main():
     clean = True
     for week_dir in glob.glob("week*/"):
-        for nb in glob.glob(f"{week_dir}*.ipynb"):
-            # extract info from name
-            prefix = os.path.splitext(os.path.basename(nb))[0]
-            week = prefix.split("_")[1]
-            date = prefix.split("_")[2]
-            # define output names
-            html_fname = f"{week_dir}{prefix}.slides.html"
-            pdf_fname = f"{week_dir}{prefix}.pdf"
-            md_fname = f"{week_dir}{prefix}.md"
-            toc_fname = f"{week_dir}{prefix}_toc.md"
-            title = f"week {week} ({date})"
-            # define commands
-            html_CMD = ["jupyter", "nbconvert", nb, "--to", "slides"]
-            md_CMD = ["jupyter", "nbconvert", nb, "--to", "markdown"]
-            pdf_CMD = ["pandoc", md_fname, "--latex-engine=xelatex", "-o",
-                       pdf_fname, "-V", "geometry:margin=1in"]
-            if clean:
+        print(week_dir)
+        os.chdir(week_dir)
+        print(os.getcwd())
+        week = os.path.dirname(week_dir).split("_")[0][4:]
+        date = os.path.dirname(week_dir).split("_")[1]
+        md_fname = f"week{week}.md"
+        print(md_fname)
+        assert os.path.isfile(md_fname)
+        # define output names
+        html_fname = f"week{week}.html"
+        pdf_fname = f"week{week}.pdf"
+        toc_fname = f"week{week}_toc.md"
+        title = f"week-{week} ({date})"
+        print(title)
+        # define commands
+        html_CMD = ["pandoc", "-t", "revealjs", "-c", "../skh.css", "-o",
+                    html_fname, md_fname, "-V", "revealjs-url=../reveal.js", "-i"]
+        pdf_CMD = ["pandoc", md_fname, "--latex-engine=xelatex", "-o",
+                   pdf_fname, "-V", "geometry:margin=1in", "--variable",
+                   "urlcolor=magenta"]
+        if clean:
+            subprocess.check_call(html_CMD)
+            subprocess.check_call(pdf_CMD)
+            make_md(title, html_fname, pdf_fname, toc_fname)
+        else:
+            if not os.path.isfile(html_fname):
                 subprocess.check_call(html_CMD)
-                subprocess.check_call(md_CMD)
-                subprocess.check_call(pdf_CMD)
-                make_md(title, nb, html_fname, pdf_fname, md_fname, toc_fname)
             else:
-                if not os.path.isfile(html_fname):
-                    subprocess.check_call(html_CMD)
-                else:
-                    print(f"Already found file {html_fname}")
-                if not os.path.isfile(pdf_fname):
-                    subprocess.check_call(md_CMD)
-                    subprocess.check_call(pdf_CMD)
-                else:
-                    print(f"Already found file {pdf_fname}")
-                if not os.path.isfile(md_fname):
-                    make_md(title, nb, html_fname, pdf_fname, md_fname, toc_fname)
-                else:
-                    print(f"Already found file {md_fname}")
-
-
+                print(f"Already found file {html_fname}")
+            if not os.path.isfile(pdf_fname):
+                subprocess.check_call(pdf_CMD)
+            else:
+                print(f"Already found file {pdf_fname}")
+        os.chdir("../")
+        print(os.getcwd())
+        print("done")
 
 if __name__ == '__main__':
     main()
